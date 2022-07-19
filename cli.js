fdb29dbe899cc3060f04680ed8450facb24d3918
Stuart Watt <stuart@morungos.com>
2022-07-19 20:48:15 -0300

    wip: added a wee bit more code

--- cli.js
+++ cli.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
 
-const [,, ...args] = process.argv
+const minimist = require('minimist')
 
-console.log("Hello world", args);
+const help = () => {
 
+}
+
+/*
+ * First step, authentication with OAuth. We may need to be able to 
+ * do this more than once, for different systems, so we can associate
+ * an access token and a refresh token with a portal.
+ */
+
+const main = () => {
+  const args = minimist(process.argv.slice(2))
+  console.log("Hello world", args);
+
+  const unprocessed = args['_']
+}
+
+main()
