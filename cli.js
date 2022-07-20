3992d30e9b3943516b994a15826a34dc126f5cc4
Stuart Watt <stuart@morungos.com>
2022-07-20 09:27:06 -0300

    wip: completed list command

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const fetch = require('node-fetch')
 const minimist = require('minimist')
 
 const authorize = require('./lib/auth')
@@ -10,8 +11,60 @@ Usage: shmt [command] [flags]
 
 Commands:
   - auth
+  - get [url] --api-key=xxxx
   - help
 `)
+}
+
+async function list(args) {
+  const unprocessed = args['_']
+  unprocessed.shift()
+
+  const apiKey = args['api-key'];
+  if (! apiKey) {
+    throw new Error("Missing API key")
+  }
+
+  const query = `https://api.hubapi.com/content/api/v2/pages?hapikey=${apiKey}`
+
+  const response = await fetch(query)
+
+  if (response.status != 200) {
+    throw new Error(`Invalid response: ${response.statusText}`)
+  }
+
+  const body = await response.json();
+  for(const page of body.objects) {
+    console.log("%s (id: %d, state: %s)", page.url, page.id, page.current_state)
+  }
+}
+
+
+
+async function get(args) {
+  const unprocessed = args['_']
+  unprocessed.shift()
+
+  const slug = unprocessed.shift()
+  if (! slug) {
+    throw new Error("Missing page to retrieve")
+  }
+
+  const apiKey = args['api-key'];
+  if (! apiKey) {
+    throw new Error("Missing API key")
+  }
+
+  const query = `https://api.hubapi.com/content/api/v2/pages?hapikey=${apiKey}`
+
+  const response = await fetch(query)
+
+  if (response.status != 200) {
+    throw new Error(`Invalid response: ${response.statusText}`)
+  }
+
+  const body = await response.json();
+  console.log("Response: %O", body)
 }
 
 /*
@@ -22,7 +75,6 @@ Commands:
 
 async function main() {
   const args = minimist(process.argv.slice(2))
-  console.log("Hello world", args);
 
   const unprocessed = args['_']
   if (unprocessed.length == 0) {
@@ -39,6 +91,14 @@ async function main() {
       await authorize()
       break;
   
+    case 'list':
+      await list(args)
+      break;
+    
+    case 'get':
+      await get(args)
+      break;
+      
     default:
       console.error(`
 Unexpected command: ${command}
@@ -51,4 +111,9 @@ Use "shmt help" for more information`
 
 }
 
-main()
+try {
+  main()
+} catch (error) {
+  console.error(error)
+  process.exit(1)
+}
